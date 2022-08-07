35c976bef7914db24e5094ed16ccd463c81b62ef
dfish19 <dfish19@yahoo.com>
2022-08-07 12:49:33 -0500

    initial

--- BasicClassesExercise/Program.cs
+++ BasicClassesExercise/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Genesis";
+            myCar.Model = "G90";
+            myCar.Year = 2018;
+
+            var honda = new Car()
+            {
+                Make = "Honda",
+                Model = "Accord",
+                Year = 2020,
+            };
+
+            var infinti = new Car("Infiniti", "QX80", 2014);
+            var carList = new List<Car>() { myCar, honda, infinti };
+            
+            foreach(var car in carList)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
+            
+        }
+    }
+}
